f90290a0cea64bdaef54b23ac64fbd71616a6b37
Soren Rasmussen <s.c.rasmussen@gmail.com>
2016-01-23 12:13:34 -0800

    Created functions and wrote tests

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a special "matrix" object that can cache it's inverse
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(matrix = matrix()) {
+   inverse <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+   ## define functions
+   setMatrix <- function(x) {
+      matrix <<- x
+      inverse <<- NULL
+   }
+   getMatrix <- function() matrix
+   setInverse <- function(i) inverse <<- i
+   getInverse <- function() inverse
+   list(setMatrix = setMatrix, getMatrix = getMatrix,
+        setInverse = setInverse, getInverse = getInverse)
+}
 
+## Computes the inverse of the matrix object. If already solved, will retrieve
+## inverse from the cache.
+
+cacheSolve <- function(m, ...) {
+   ## Return a matrix that is the inverse of 'x'
+   inverse <- m$getInverse()
+   if(!is.null(inverse)) {
+      message("getting cached inverse")
+      return(inverse)
+   }
+   matrix <- m$getMatrix()
+   inverse <- solve(matrix)
+   message("solving and caching inverse")
+   m$setInverse(inverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##
+## Testing the Functions
+
+n <- 4
+m <- matrix(rnorm(n*n),n,n)
+
+cat("Original Matrix:\n")
+m
+matrix <- makeCacheMatrix(m)
+
+cat("\ngetMatrix() function result:\n")
+matrix$getMatrix()
+
+cat("\ngetInverse() function before cacheSolve is called, should be NULL:\n")
+matrix$getInverse()
+
+cat("\nCalling cacheSolve the first time:\n")
+cacheSolve(matrix)
+
+cat("\nCalling cacheSolve the second time:\n")
+cacheSolve(matrix)
+
+
+
+
+
+
+
